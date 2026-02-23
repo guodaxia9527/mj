ce85dfd562ecaa646a4ce3465551a14366b11903
guodaxia9527 <50985238+guodaxia9527@users.noreply.github.com>
2026-02-24 00:44:08 +0800

    添加图标支持

---
+++ sw.js
@@ -0,0 +1,19 @@
+const CACHE_NAME = 'mj-v8-cache'; // 升级版本号
+const ASSETS = [
+    './index.html',
+    './manifest.json',
+    './sw.js'
+];
+
+self.addEventListener('install', (e) => {
+    e.waitUntil(
+        caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
+    );
+    self.skipWaiting(); // 强制跳过等待，立即激活
+});
+
+self.addEventListener('fetch', (e) => {
+    e.respondWith(
+        caches.match(e.request).then(res => res || fetch(e.request))
+    );
+});
